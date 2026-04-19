e6abd538a05f2ab9e9a7234db61500aa0573fe6e
Hélder Ferreira <ferreirahelder80@gmail.com>
2026-04-19 07:45:41 +0000

    Bluelink PRO: adiciona link Drive, cache v8

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker — Pisa-Papéis
-const CACHE = 'pisa-papeis-v7';
+const CACHE = 'pisa-papeis-v8';
 const ASSETS = ['./index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
