355f67b8f16b9498116c2478f024605f60a4d92f
Helder Ferreira <ferreirahelder80@gmail.com>
2026-04-29 12:59:25 +0000

    Add Antonio Vieira (Abr) + Galp Mai; bump cache v14

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker — Pisa-Papéis
-const CACHE = 'pisa-papeis-v13';
+const CACHE = 'pisa-papeis-v14';
 const ASSETS = ['./index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
