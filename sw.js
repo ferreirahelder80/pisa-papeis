136d7dcdf41b88484ee46c92631da30d1b7a0fcc
Hélder Ferreira <ferreirahelder80@gmail.com>
2026-05-03 13:37:11 +0000

    Atualização 3 Mai 2026: +7 docs (InWatt, Jani-King 6885, Tranquilidade Acid Trab, AntonioVieira FA+RC), cache v16

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker — Pisa-Papéis
-const CACHE = 'pisa-papeis-v15';
+const CACHE = 'pisa-papeis-v16';
 const ASSETS = ['./index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
