b5f126ebe3fd79677a397b70ac4848b29b7c0698
Hélder Ferreira <ferreirahelder80@gmail.com>
2026-05-03 13:54:56 +0000

    Recibos CPC Maio 2026: +1 doc (Cristiana Barbosa TF); cache v18

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker — Pisa-Papéis
-const CACHE = 'pisa-papeis-v17';
+const CACHE = 'pisa-papeis-v18';
 const ASSETS = ['./index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
