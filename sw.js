230b681043e2bf045605d9f3b7e81a97a96c16ac
Hélder Ferreira <ferreirahelder80@gmail.com>
2026-07-28 06:15:51 +0000

    Sync Pisa-Papéis 28 Jul 2026: +10 docs Julho (5 Faturas ZC, 4 Pag Pessoais, 1 Acordo Parental); cache v32

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker — Pisa-Papéis
-const CACHE = 'pisa-papeis-v31';
+const CACHE = 'pisa-papeis-v32';
 const ASSETS = ['./index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
