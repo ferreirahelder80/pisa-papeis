ab3e5203289e7a24748975071ad0dcf249b9078f
Hélder Ferreira <ferreirahelder80@gmail.com>
2026-08-19 23:03:10 +0000

    Update Ago/Set 2026: Jani-King FT 14058, Lusitania PPR Ago, Manus Pro Ago, Manie/SU/SMAS Viseu, Ageas Set amounts, cache v42

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker — Pisa-Papéis
-const CACHE = 'pisa-papeis-v41';
+const CACHE = 'pisa-papeis-v42';
 const ASSETS = ['./index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
