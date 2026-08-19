570bfd91358e8536e1741e3c224b0f29bbc1c3b5
Hélder Ferreira <ferreirahelder80@gmail.com>
2026-08-19 23:12:53 +0000

    Subs ZC/Ago: +Perplexity Enterprise Credits USD 61.50 (EUR 52.99), cache v43

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker — Pisa-Papéis
-const CACHE = 'pisa-papeis-v42';
+const CACHE = 'pisa-papeis-v43';
 const ASSETS = ['./index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
